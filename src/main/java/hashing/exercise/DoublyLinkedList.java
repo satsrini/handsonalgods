277b00a948e0271f40b76b6aaa56cf8c99a5321f
root <root@ev2.us-west4-b.c.tonal-nova-323007.internal>
2021-09-11 17:44:34 +0000

    implementing put() and get() for DoublyLinkedList

--- src/main/java/hashing/exercise/DoublyLinkedList.java
+++ src/main/java/hashing/exercise/DoublyLinkedList.java
@@ -16,6 +16,62 @@ public class DoublyLinkedList<Key extends Comparable<Key>, Value>
     public void put(Key key, Value value)
     {
 
+       Node current = get(key,head);
+
+       if(current != null) // key already exists, so updating the value
+       {
+           current.value = value;
+           return;
+       }
+
+       Node newNode = new Node(key,value);
+
+       if(size() == 0) // first entry
+       {
+           head = newNode; 
+       }else
+       if(size() == 1)  // second entry
+       {
+           newNode.next = head;
+           head.previous = newNode;
+           tail = head;
+           head = newNode;
+       }else
+       {
+           newNode.next = head;
+           head.previous = newNode;
+           head = newNode;
+       }
+
+       N++;
+    }
+
+    public Value get(Key key)
+    {
+        Node current = get(key,head);
+
+        if(current == null)
+        {
+            return null;
+        }
+
+        return current.value;
+    }
+
+    private Node get(Key key, Node current)
+    {
+ 
+        while(current != null)
+        {
+            if(key.compareTo(current.key) == 0)
+            {
+                return current;
+            }
+            
+            current = current.next;
+        }
+
+        return null;
     }
 
     public boolean isEmpty()
@@ -32,8 +88,8 @@ public class DoublyLinkedList<Key extends Comparable<Key>, Value>
     {
        private Key key;
        private Value value;
-       private Node previousNode;
-       private Node nextNode;
+       private Node previous;
+       private Node next;
 
        private Node(Key key, Value value){
            this.key = key;
